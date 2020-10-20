93bd16d5fe01deb68a2753e3b49c3c7df900cd39
Osetinskiy-pokemon <71440118+Osetinskiy-pokemon@users.noreply.github.com>
2020-10-20 10:51:30 +0300

    Delete Main.java

--- Game_TicTacToe/Main.java
+++
@@ -1,9 +0,0 @@
-package Game_TicTacToe;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Самое время повеселиться! Игра начинается!");
-        TicTacToe newGame = new TicTacToe();
-        newGame.game();
-    }
-}
